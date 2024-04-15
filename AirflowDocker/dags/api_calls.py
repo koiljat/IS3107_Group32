1bf8fce45f3044f1e313c08089e1ed758864dded
Gemson14 <98260433+Gemson14@users.noreply.github.com>
2024-04-15 22:38:42 +0800

    Removed hidden values, merged both APIs, Added upload to BQ func

--- AirflowDocker/dags/api_calls.py
+++ AirflowDocker/dags/api_calls.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from airflow.decorators import dag, task
 from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from google.cloud import bigquery
 import time
 import pandas as pd
@@ -94,36 +95,38 @@ def api_calls_taskflow():
                 trim_info = get_trim_details(make, model, year)
                 if trim_info:
                     query_temp_dict['model_make_id'] = trim_info['make_model_id']
-                    query_temp_dict['model_name'] = trim_info['make_model']['name']
                     query_temp_dict['model_trim'] = trim_info['id']
                     query_temp_dict['model_year'] = trim_info['year']
-                    query_temp_dict['model_body'] = trim_info['make_model_trim_body']['type']
                     query_temp_dict['model_seats'] = trim_info['make_model_trim_body']['seats']
                     query_temp_dict['model_weight_kg'] = trim_info['make_model_trim_body']['curb_weight']
-                    query_temp_dict['model_engine_fuel'] = trim_info['make_model_trim_engine']['fuel_type']
-                    query_temp_dict['model_engine_cyl'] = trim_info['make_model_trim_engine']['cylinders']
-                    query_temp_dict['model_drive'] = trim_info['make_model_trim_engine']['drive_type']
-                    query_temp_dict['model_transmission_type'] = trim_info['make_model_trim_engine']['transmission']
                     query_temp_dict['model_fuel_cap_l'] = trim_info['make_model_trim_mileage']['fuel_tank_capacity']
-                
-                else:
-                    # Can consider implementing individual column checks to populate with second API
+                    #if theres no hidden data, js assign immediately, if not remain empty
+                    if "hidden" not in trim_info['make_model']['name']:
+                        query_temp_dict['model_name'] = model
+                        query_temp_dict['model_body'] = trim_info['make_model_trim_body']['type']
+                        query_temp_dict['model_engine_fuel'] = trim_info['make_model_trim_engine']['fuel_type']
+                        query_temp_dict['model_engine_cyl'] = trim_info['make_model_trim_engine']['cylinders']
+                        query_temp_dict['model_drive'] = trim_info['make_model_trim_engine']['drive_type']
+                        query_temp_dict['model_transmission_type'] = trim_info['make_model_trim_engine']['transmission']
+                    
+                # call carQuery if there are null values in the row        
+                if not all(value is not None for value in query_temp_dict.values()):
                      
                     model_info = get_car_query_trims(make, model, year)
                     if model_info:
                         first_trim = model_info[0]
-                        query_temp_dict['model_make_id'] = first_trim['model_make_id']
-                        query_temp_dict['model_name'] = first_trim['model_name']
-                        query_temp_dict['model_trim'] = first_trim['model_trim']
-                        query_temp_dict['model_year'] = first_trim['model_year']
-                        query_temp_dict['model_body'] = first_trim['model_body']
-                        query_temp_dict['model_seats'] = first_trim['model_seats']
-                        query_temp_dict['model_weight_kg'] = first_trim['model_weight_kg']
-                        query_temp_dict['model_engine_fuel'] = first_trim['model_engine_fuel']
-                        query_temp_dict['model_engine_cyl'] = first_trim['model_engine_cyl']
-                        query_temp_dict['model_drive'] = first_trim['model_drive']
-                        query_temp_dict['model_transmission_type'] = first_trim['model_transmission_type']
-                        query_temp_dict['model_fuel_cap_l'] = first_trim['model_fuel_cap_l']
+                        query_temp_dict['model_make_id'] = query_temp_dict.get('model_make_id', first_trim['model_make_id'])
+                        query_temp_dict['model_name'] = query_temp_dict.get('model_name', first_trim['model_name'])
+                        query_temp_dict['model_trim'] = query_temp_dict.get('model_trim', first_trim['model_trim'])
+                        query_temp_dict['model_year'] = query_temp_dict.get('model_year', first_trim['model_year'])
+                        query_temp_dict['model_body'] = query_temp_dict.get('model_body', first_trim['model_body'])
+                        query_temp_dict['model_seats'] = query_temp_dict.get('model_seats', first_trim['model_seats'])
+                        query_temp_dict['model_weight_kg'] = query_temp_dict.get('model_weight_kg', first_trim['model_weight_kg'])
+                        query_temp_dict['model_engine_fuel'] = query_temp_dict.get('model_engine_fuel', first_trim['model_engine_fuel'])
+                        query_temp_dict['model_engine_cyl'] = query_temp_dict.get('model_engine_cyl', first_trim['model_engine_cyl'])
+                        query_temp_dict['model_drive'] = query_temp_dict.get('model_drive', first_trim['model_drive'])
+                        query_temp_dict['model_transmission_type'] = query_temp_dict.get('model_transmission_type', first_trim['model_transmission_type'])
+                        query_temp_dict['model_fuel_cap_l'] = query_temp_dict.get('model_fuel_cap_l', first_trim['model_fuel_cap_l'])
                         
                 # Update dict
                 all_values_none = all(value is None for value in query_temp_dict.values()) # if both APIS failed
@@ -160,36 +163,38 @@ def api_calls_taskflow():
                 trim_info = get_trim_details(make, model, year)
                 if trim_info:
                     query_temp_dict['model_make_id'] = trim_info['make_model_id']
-                    query_temp_dict['model_name'] = trim_info['make_model']['name']
                     query_temp_dict['model_trim'] = trim_info['id']
                     query_temp_dict['model_year'] = trim_info['year']
-                    query_temp_dict['model_body'] = trim_info['make_model_trim_body']['type']
                     query_temp_dict['model_seats'] = trim_info['make_model_trim_body']['seats']
                     query_temp_dict['model_weight_kg'] = trim_info['make_model_trim_body']['curb_weight']
-                    query_temp_dict['model_engine_fuel'] = trim_info['make_model_trim_engine']['fuel_type']
-                    query_temp_dict['model_engine_cyl'] = trim_info['make_model_trim_engine']['cylinders']
-                    query_temp_dict['model_drive'] = trim_info['make_model_trim_engine']['drive_type']
-                    query_temp_dict['model_transmission_type'] = trim_info['make_model_trim_engine']['transmission']
                     query_temp_dict['model_fuel_cap_l'] = trim_info['make_model_trim_mileage']['fuel_tank_capacity']
-                
-                else:
-                    # Can consider implementing individual column checks to populate with second API
+                    #if theres no hidden data, js assign immediately, if not remain empty
+                    if "hidden" not in trim_info['make_model']['name']:
+                        query_temp_dict['model_name'] = model
+                        query_temp_dict['model_body'] = trim_info['make_model_trim_body']['type']
+                        query_temp_dict['model_engine_fuel'] = trim_info['make_model_trim_engine']['fuel_type']
+                        query_temp_dict['model_engine_cyl'] = trim_info['make_model_trim_engine']['cylinders']
+                        query_temp_dict['model_drive'] = trim_info['make_model_trim_engine']['drive_type']
+                        query_temp_dict['model_transmission_type'] = trim_info['make_model_trim_engine']['transmission']
+                    
+                # call carQuery if there are null values in the row        
+                if not all(value is not None for value in query_temp_dict.values()):
                      
                     model_info = get_car_query_trims(make, model, year)
                     if model_info:
                         first_trim = model_info[0]
-                        query_temp_dict['model_make_id'] = first_trim['model_make_id']
-                        query_temp_dict['model_name'] = first_trim['model_name']
-                        query_temp_dict['model_trim'] = first_trim['model_trim']
-                        query_temp_dict['model_year'] = first_trim['model_year']
-                        query_temp_dict['model_body'] = first_trim['model_body']
-                        query_temp_dict['model_seats'] = first_trim['model_seats']
-                        query_temp_dict['model_weight_kg'] = first_trim['model_weight_kg']
-                        query_temp_dict['model_engine_fuel'] = first_trim['model_engine_fuel']
-                        query_temp_dict['model_engine_cyl'] = first_trim['model_engine_cyl']
-                        query_temp_dict['model_drive'] = first_trim['model_drive']
-                        query_temp_dict['model_transmission_type'] = first_trim['model_transmission_type']
-                        query_temp_dict['model_fuel_cap_l'] = first_trim['model_fuel_cap_l']
+                        query_temp_dict['model_make_id'] = query_temp_dict.get('model_make_id', first_trim['model_make_id'])
+                        query_temp_dict['model_name'] = query_temp_dict.get('model_name', first_trim['model_name'])
+                        query_temp_dict['model_trim'] = query_temp_dict.get('model_trim', first_trim['model_trim'])
+                        query_temp_dict['model_year'] = query_temp_dict.get('model_year', first_trim['model_year'])
+                        query_temp_dict['model_body'] = query_temp_dict.get('model_body', first_trim['model_body'])
+                        query_temp_dict['model_seats'] = query_temp_dict.get('model_seats', first_trim['model_seats'])
+                        query_temp_dict['model_weight_kg'] = query_temp_dict.get('model_weight_kg', first_trim['model_weight_kg'])
+                        query_temp_dict['model_engine_fuel'] = query_temp_dict.get('model_engine_fuel', first_trim['model_engine_fuel'])
+                        query_temp_dict['model_engine_cyl'] = query_temp_dict.get('model_engine_cyl', first_trim['model_engine_cyl'])
+                        query_temp_dict['model_drive'] = query_temp_dict.get('model_drive', first_trim['model_drive'])
+                        query_temp_dict['model_transmission_type'] = query_temp_dict.get('model_transmission_type', first_trim['model_transmission_type'])
+                        query_temp_dict['model_fuel_cap_l'] = query_temp_dict.get('model_fuel_cap_l', first_trim['model_fuel_cap_l'])
                         
                 # Update dict
                 all_values_none = all(value is None for value in query_temp_dict.values()) # if both APIS failed
@@ -225,7 +230,6 @@ def api_calls_taskflow():
         csv_data = df.to_csv(index=False).encode()
     
         gcs_hook.upload(bucket_name=bucket_name, object_name=object_name, data=csv_data)
-
         
         
     motorist_df = fetch_motorist()
@@ -235,6 +239,19 @@ def api_calls_taskflow():
     second_dict = execute_api_calls_for_sgcarmart(sgcarmart_df, first_dict)
     save_api_json(second_dict)
     save_api_csv(second_dict)
+    load_csv_to_bigquery = GCSToBigQueryOperator(
+        task_id='load_csv_to_bigquery',
+        bucket='is3107-datasets',
+        source_objects=['carAPI/api.csv'],
+        destination_project_dataset_table='api.api',
+        source_format='CSV',
+        skip_leading_rows=1,
+        write_disposition='WRITE_TRUNCATE',
+        gcp_conn_id='google_cloud_default',
+        autodetect=True
+    )
+    
+    save_api_csv(second_dict) >> load_csv_to_bigquery
     
 
 dag = api_calls_taskflow()
